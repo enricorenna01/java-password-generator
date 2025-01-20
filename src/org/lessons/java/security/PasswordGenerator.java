8dc140b3f7d1ba7b89bdaa38528447b49cc9f8d3
enricorenna01 <erenna39@gmail.com>
2025-01-20 18:46:41 +0100

    feat: added variables

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,29 @@
 package org.lessons.java.security;
+import java.util.Scanner;
 
 public class PasswordGenerator {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Inserisci il tuo nome e premi invio...");
+        String firstName = input.nextLine();
 
+        System.out.println("Inserisci il tuo cognome e premi invio...");
+        String secondName = input.nextLine();
+
+        System.out.println("Inserisci il tuo colore preferito e premi invio...");
+        String favouriteColor = input.nextLine();
+
+        System.out.println("Inserisci il giorno della tua nacita e premi invio...");
+        int dayOfBirth = Integer.parseInt(input.nextLine());
+
+        System.out.println("Inserisci il mese della tua nacita e premi invio...");
+        int monthOfBirth = Integer.parseInt(input.nextLine());
+
+        System.out.println("Inserisci l'anno della tua nacita e premi invio...");
+        int yearOfBirth = Integer.parseInt(input.nextLine());
+
+        input.close();
+    }
 }
